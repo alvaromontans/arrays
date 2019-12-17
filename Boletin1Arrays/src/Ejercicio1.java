4eaa6b9f1c8509b5dd0c33060450104c313fbc2c
FP <FP@PC20>
2019-12-17 14:18:18 +0100

    gg

--- Boletin1Arrays/src/Ejercicio1.java
+++ Boletin1Arrays/src/Ejercicio1.java
@@ -2,8 +2,15 @@ import java.util.Scanner;
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String texto="";
+		
+		obtenerCadenaTexto(texto);
+	}
 
+	private static void obtenerCadenaTexto(String texto) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce un texto");
+		texto = sc.nextLine();
 	}
 
 }
